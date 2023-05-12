2ba59930127a03a8a927c2c36d82d453bf81fc8e
philhuan <hjw@hjwblog.com>
2023-05-13 05:49:57 +0800

    docs: add gorm.Dialector

--- interfaces.go
+++ interfaces.go
@@ -10,13 +10,22 @@ import (
 
 // Dialector GORM database dialector
 type Dialector interface {
+	// Name 返回使用该 Dialector 实例连接的数据库类型的名称，例如 "mysql"、"sqlite" 等。
 	Name() string
+	// Initialize 用于初始化连接到数据库的 *DB 实例。此方法将在 Open 方法中调用。
 	Initialize(*DB) error
+	// Migrator 返回用于执行数据库迁移的 Migrator 接口实例。
 	Migrator(db *DB) Migrator
+	// DataTypeOf 返回给定 schema.Field 类型的数据库原生数据类型。例如，schema.Field 类型 string 可能映射到数据库中的 VARCHAR 类型。
 	DataTypeOf(*schema.Field) string
+	// DefaultValueOf 返回给定 schema.Field 类型的默认值表达式。如果该字段没有默认值，则返回 nil。
 	DefaultValueOf(*schema.Field) clause.Expression
+	// BindVarTo BindVarTo 将给定的值绑定到 SQL 语句中的占位符。
+	// 例如，BindVarTo 方法可以将值 42 绑定到 SQL 语句 SELECT * FROM users WHERE age = ? 中的 ? 占位符上。
 	BindVarTo(writer clause.Writer, stmt *Statement, v interface{})
+	// QuoteTo 将给定的标识符（例如表名、列名等）引用为数据库原生的语法。例如，在 MySQL 中引用表名 users 可能需要将其引用为 `users`。
 	QuoteTo(clause.Writer, string)
+	// Explain 返回用于解释和优化 SQL 语句的字符串，可以用于调试和优化 SQL 查询。
 	Explain(sql string, vars ...interface{}) string
 }
 
